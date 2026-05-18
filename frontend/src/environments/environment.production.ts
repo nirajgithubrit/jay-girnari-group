3aec75edc823937a56ee85cd0aabecdeb3c84b16
niraj-kumar2 <126452817+niraj-kumar2@users.noreply.github.com>
2026-05-19 00:54:15 +0530

    Add backend url in environment

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://jay-girnari-group-api.onrender.com/api',
+  apiUrl: 'https://jay-girnari-group.onrender.com/api',
   vapidPublicKey: 'BOCS9JpA3fDzTCcgvc-RcQZ828Hn-cnao32wAP_GG13hucbtwlDnhydy6V9bMdrpLG3kGwkinao0zIcraMP5Bm4',
 };
